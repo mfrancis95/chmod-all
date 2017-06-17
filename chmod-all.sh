4d74d7a807cbc0e644549327689f1488914c436a
mfrancis95 <mikefrancis95@gmail.com>
2017-06-16 20:16:26 -0400

    Initial commit

---
+++ chmod-all.sh
@@ -0,0 +1,28 @@
+show_usage() {
+    echo "Usage: chmod-all [OPTION] permissions directory"
+    echo -e "Where OPTION is one of:\n"
+    echo -e "  -d, --directories\tApply permissions to directories only."
+    echo -e "  -f, --files\t\tApply permissions to files only."
+}
+
+if [ $# -lt 2 ]; then
+    show_usage
+else
+    if [ "$1" == "-d" ] || [ "$1" == "--directories" ]; then
+        if [ $# -lt 3 ]; then
+            show_usage
+        else
+            find $3 -type d -exec chmod $2 {} \;
+        fi
+    elif [ "$1" == "-f" ] || [ "$1" == "--files" ]; then
+        if [ $# -lt 3 ]; then
+            show_usage
+        else
+            find $3 -type f -exec chmod $2 {} \;
+        fi
+    elif [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+        show_usage
+    else
+        chmod -R $1 $2
+    fi
+fi
