bafc88301068cca20fff3ca4cd0b475cd9a033ba
mfrancis95 <mikefrancis95@gmail.com>
2017-12-16 22:55:28 -0500

    Use the current working directory when the directory argument is omitted

--- chmod-all.sh
+++ chmod-all.sh
@@ -1,8 +1,9 @@
 show_usage() {
-    echo "Usage: chmod-all [OPTION] permissions directory"
-    echo -e "Where OPTION is one of:\n"
-    echo -e "  -d, --directories\tApply permissions to directories only."
-    echo -e "  -f, --files\t\tApply permissions to files only."
+    echo 'Usage: chmod-all [OPTION] permissions [directory]'
+    echo -e 'Where OPTION is one of:\n'
+    echo -e '  -d, --directories\tApply permissions to directories only.'
+    echo -e '  -f, --files\t\tApply permissions to files only.\n'
+    echo 'An omitted directory argument will default to the current working directory.'
 }
 
 if [ $# -eq 0 ]; then
@@ -14,18 +15,30 @@ else
             show_usage
             exit 1
         else
-            find $3 -type d -exec chmod $2 {} \;
+            if [ -z $3 ]; then
+                find . -type d -exec chmod $2 {} \;
+            else
+                find $3 -type d -exec chmod $2 {} \;
+            fi
         fi
     elif [ "$1" == "-f" ] || [ "$1" == "--files" ]; then
         if [ $# -lt 2 ]; then
             show_usage
             exit 1
         else
-            find $3 -type f -exec chmod $2 {} \;
+            if [ -z $3 ]; then
+                find . -type f -exec chmod $2 {} \;
+            else
+                find $3 -type f -exec chmod $2 {} \;
+            fi
         fi
     elif [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
         show_usage
     else
-        chmod -R $1 $2
+        if [ -z $2 ]; then
+            chmod -R $1 .
+        else
+            chmod -R $1 $2
+        fi
     fi
 fi
